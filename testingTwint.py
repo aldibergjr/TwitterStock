1b383c41bd5f836c4968b0b2dca87dbdd9d88d8a
acrc2 <acrc2@cin.ufpe.br>
2020-11-20 22:51:54 -0300

    save results from pandas into a json

--- testingTwint.py
+++ testingTwint.py
@@ -1,9 +1,24 @@
 import twint
+import pandas as pd
+import json
+
 
 # Configure
 c = twint.Config()
 c.Username = "realDonaldTrump"
 c.Search = "count"
+c.Limit = 20
+c.Pandas = True
+c.Hide_output = True
+
 
 # Run
 twint.run.Search(c)
+
+Tweets_df =  twint.storage.panda.Tweets_df
+result = Tweets_df.to_json(orient="records")
+parsed = json.loads(result)
+  
+f = open("output.json", "w")
+f.write(json.dumps(parsed, indent=4))
+f.close()
