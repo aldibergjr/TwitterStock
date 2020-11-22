5c02708d87d09dce9479e6c5a60b6304536d571e
cdbm <cdbm@cin.ufpe.br>
2020-11-22 14:39:50 -0300

    servico de tweets pronto (?)

--- resources/twint-api/twintApi.py
+++ resources/twint-api/twintApi.py
@@ -1,14 +1,25 @@
 from flask import Flask
 import twitter_worker
-from concurrent.futures import ProcessPoolExecutor
+from pebble import ProcessPool
 
 app = Flask(__name__)
+processes = []
 
 @app.route('/<query>')
 def hello_world(query):
-    executor = ProcessPoolExecutor()
-    executor.submit(twitter_worker.getTweets, query)
+    executor = ProcessPool()
+    future = executor.schedule(twitter_worker.getTweets, args=[query])
+    processes.append({'process': future, 'query':query})
+    print(processes)
     return {'success' : 'ack'}
 
+@app.route('/cancel/<query>')
+def cancel_query(query):
+    for pross in processes:
+        if(pross["query"] == query):
+            pross["process"].cancel()
+            processes.remove(pross)
+            print(processes)
 
+    return {'sucess' : 'removed'}
 
